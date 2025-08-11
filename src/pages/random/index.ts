a5dea0ae30c6c8d71f27ed15d3c45eda76b07b17
FEAR939 <rogronin8877@gmail.com>
2025-08-12 00:58:29 +0200

    added page

---
+++ src/pages/random/index.ts
@@ -0,0 +1,44 @@
+import { getSearch } from "../../lib/anilist/index";
+
+export default async function Random(query) {
+  const page = document.createElement("div");
+  page.className =
+    "relative h-full w-full px-12 pb-4 space-y-4 overflow-y-scroll";
+
+  document.root.appendChild(page);
+
+  const searchBar = document.createElement("input");
+  searchBar.className = "px-2 py-4 rounded-xl bg-[#0c0c0c] text-white";
+
+  page.appendChild(searchBar);
+
+  searchBar.addEventListener("keyup", async (e) => {
+    if (e.keyCode == 13) {
+      const results = await getSearch(searchBar.value);
+      return showSearch(results);
+    }
+  });
+
+  const resultContainer = document.createElement("div");
+  resultContainer.className = "grid grid-cols-1 gap-4";
+
+  page.appendChild(resultContainer);
+
+  function showSearch(results) {
+    resultContainer.innerHTML = "";
+    if (!results) return;
+
+    results.map((result) => {
+      const resultCard = document.createElement("div");
+      resultCard.className = "bg-[#0c0c0c] rounded-xl p-4";
+      resultCard.innerHTML = `
+        <h2 class="text-white">${result.title.romaji}</h2>
+      `;
+      resultContainer.appendChild(resultCard);
+
+      resultCard.addEventListener("click", () => {
+        document.router.navigate(`/anime?id=${result.id}`);
+      });
+    });
+  }
+}
