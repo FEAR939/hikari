8b0661add58742a68add08be0eb4794a3e512555
FEAR939 <rogronin8877@gmail.com>
2025-08-24 04:34:09 +0200

    added proxy for android

--- src/app.ts
+++ src/app.ts
@@ -9,18 +9,48 @@ import Settings from "./pages/settings/index";
 import Auth from "./pages/auth/index";
 import { topBar } from "./ui/topbar";
 
-const root = document.getElementById("root");
-document.root = root;
+declare global {
+  interface Document {
+    root: HTMLElement;
+  }
+}
 
-document.body.appendChild(topBar());
+function main() {
+  const root = document.getElementById("root")!;
+  document.root = root;
 
-router.route("/", Home);
-router.route("/anime", Anime);
-router.route("/player", Player);
-router.route("/search", Search);
-router.route("/settings", Settings);
-router.route("/auth", Auth);
+  document.body.appendChild(topBar());
 
-router.navigate("/");
+  router.route("/", Home);
+  router.route("/anime", Anime);
+  router.route("/player", Player);
+  router.route("/search", Search);
+  router.route("/settings", Settings);
+  router.route("/auth", Auth);
 
-authService.authenticate();
+  router.navigate("/");
+
+  authService.authenticate();
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  initProxy();
+});
+
+function initProxy() {
+  if (typeof window.ensureAndroidProxyReady === "function") {
+    window.ensureAndroidProxyReady().then(() => {
+      main();
+    });
+  } else {
+    console.warn(
+      "initializeApp: window.ensureAndroidProxyReady is not defined. Network proxying may not work.",
+    );
+
+    console.log(
+      "initializeApp: Proceeding without proxy. If you are on android expect CORS issues for external fetches, otherwise you are likely on desktop so just ignore this.",
+    );
+
+    main();
+  }
+}
