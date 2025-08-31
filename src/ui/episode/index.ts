9ef6125232eaf77d9dfeee726d237b5d07192d19
FEAR939 <rogronin8877@gmail.com>
2025-08-31 05:00:39 +0200

    added air date to UI

--- src/ui/episode/index.ts
+++ src/ui/episode/index.ts
@@ -22,7 +22,7 @@ interface Episode {
 export default function Episode(episode: Episode, index: number): EpElement {
   const episodeCard: EpElement = document.createElement("div");
   episodeCard.className =
-    "relative h-24 md:h-32 w-full bg-[#0c0c0c] flex overflow-hidden rounded-xl cursor-pointer";
+    "h-24 md:h-32 w-full bg-[#0c0c0c] flex overflow-hidden rounded-xl cursor-pointer";
 
   if (episode.image) {
     const episodeCardImage = document.createElement("img");
@@ -33,10 +33,10 @@ export default function Episode(episode: Episode, index: number): EpElement {
 
   const episodeSide = document.createElement("div");
   episodeSide.className =
-    "h-full w-full space-y-1 md:space-y-4 p-4 overflow-hidden";
+    "relative h-full w-full space-y-1 md:space-y-4 p-4 overflow-hidden";
 
   const episodeTitle = document.createElement("h2");
-  episodeTitle.className = "text-l md:text-xl font-bold truncate";
+  episodeTitle.className = "text-sm md:text-xl font-bold truncate";
   episodeTitle.textContent =
     `${episode.episode}. ${episode.title.en || ""}` || `Episode ${index + 1}`;
   episodeSide.appendChild(episodeTitle);
@@ -46,6 +46,20 @@ export default function Episode(episode: Episode, index: number): EpElement {
   episodeDescription.textContent = episode.overview;
   episodeSide.appendChild(episodeDescription);
 
+  const episodeAirDate = document.createElement("div");
+  episodeAirDate.className =
+    "absolute bottom-4 left-4 text-xs m-0 text-[#6f6f6f]";
+
+  const inFuture = episode.airdate > new Date().toISOString().split("T")[0];
+
+  if (inFuture) {
+    episodeAirDate.textContent = `Airs on ${episode.airdate}`;
+  } else {
+    episodeAirDate.textContent = `Aired on ${episode.airdate}`;
+  }
+
+  episodeSide.appendChild(episodeAirDate);
+
   const episodeSource = document.createElement("div");
   episodeSource.className = "absolute bottom-4 right-4";
   episodeSource.innerHTML = `<svg aria-hidden="true" class="w-4 h-4 text-[#f0f0f0] animate-spin dark:text-[#0d0d0d] fill-[#a0a0a0]" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -53,7 +67,7 @@ export default function Episode(episode: Episode, index: number): EpElement {
           <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
       </svg>`;
 
-  episodeCard.appendChild(episodeSource);
+  episodeSide.appendChild(episodeSource);
 
   episodeCard.updateSource = function (source: Source | boolean) {
     if (!source && typeof source == "boolean") {
