d2e17b37c5cd51c87dff65bf6a62ade84d2554e2
FEAR939 <rogronin8877@gmail.com>
2025-09-11 00:18:50 +0200

    UI enhancements

--- src/ui/sourcePanel/index.ts
+++ src/ui/sourcePanel/index.ts
@@ -2,14 +2,14 @@ import { getProvider, getEpisode } from "../../lib/animetoast";
 import { getMetadata } from "../../lib/voe";
 import { router } from "../../lib/router/index";
 
-export function SourcePanel(anime, episode) {
+export function SourcePanel(anime, episodes, index) {
   const container = document.createElement("div");
   container.className =
     "fixed inset-0 p-4 flex items-center justify-center backdrop-brightness-50 backdrop-blur-md";
 
   const panel = document.createElement("div");
   panel.className =
-    "relative w-full max-w-4xl h-2/3 p-4 pt-12 overflow-y-scroll bg-[#080808] rounded-xl";
+    "relative w-full max-w-4xl h-2/3 p-4 pt-8 space-y-4 overflow-y-scroll bg-[#080808] rounded-xl";
 
   container.appendChild(panel);
 
@@ -24,10 +24,57 @@ export function SourcePanel(anime, episode) {
     container.remove();
   });
 
+  const animeBanner = document.createElement("img");
+  animeBanner.className =
+    "absolute top-0 left-0 right-0 w-full h-32 object-cover brightness-50 mask-b-from-50% bg-[#080808]";
+  animeBanner.src = anime.bannerImage || anime.trailer.thumbnail;
+
+  panel.appendChild(animeBanner);
+
+  const animeName = document.createElement("div");
+  animeName.className = "relative text-2xl font-semibold text-white";
+  animeName.textContent = anime.title.romaji;
+
+  panel.appendChild(animeName);
+
+  const episodePicker = document.createElement("div");
+  episodePicker.className = "relative h-fit w-full space-y-2";
+
+  panel.appendChild(episodePicker);
+
+  const episodePickerLabel = document.createElement("div");
+  episodePickerLabel.className = "text-base text-white";
+  episodePickerLabel.textContent = "Episode";
+
+  episodePicker.appendChild(episodePickerLabel);
+
+  const episodePickerInput = document.createElement("input");
+  episodePickerInput.className =
+    "w-full px-4 py-2 bg-[#080808] outline-1 outline-[#1a1a1a] rounded-md";
+  episodePickerInput.type = "number";
+  episodePickerInput.min = "1";
+  episodePickerInput.max = episodes.length;
+  episodePickerInput.value = (index + 1).toString();
+
+  episodePicker.appendChild(episodePickerInput);
+
+  episodePickerInput.addEventListener("input", () => {
+    const value = parseInt(episodePickerInput.value);
+
+    if (isNaN(value) || value < 1 || value > episodes.length) return;
+
+    index = value - 1;
+
+    loadSource();
+  });
+
+  console.log(episodes, index);
+
   const extensions = {
     source: [
       {
         name: "AnimeToast",
+        icon: "https://www.animetoast.cc/wp-content/uploads/2018/03/toastfavi-72x72.png",
       },
     ],
     stream: [
@@ -42,100 +89,106 @@ export function SourcePanel(anime, episode) {
 
   panel.appendChild(sourceElementList);
 
-  extensions.source.map(async (source_extension) => {
-    const sourceElement = document.createElement("div");
-    sourceElement.className = "relative h-32 w-full p-4 space-y-2";
+  loadSource();
 
-    const sourceName = document.createElement("div");
-    sourceName.className = "text-white font-bold";
-    sourceName.textContent = source_extension.name;
+  function loadSource() {
+    sourceElementList.innerHTML = "";
 
-    sourceElement.appendChild(sourceName);
+    extensions.source.map(async (source_extension) => {
+      const skeletonElement = document.createElement("div");
+      skeletonElement.className =
+        "relative w-full h-28 p-4 outline-1 outline-[#1a1a1a] rounded-md cursor-pointer space-y-2 animate-pulse";
+      skeletonElement.innerHTML = `
+        <div class="mt-2 w-1/3 h-4 bg-[#1a1a1a] rounded-md"></div>
+        <div class="w-2/3 h-4 bg-[#1a1a1a] rounded-md"></div>
+        <div class="mt-2 w-1/4 h-4 bg-[#1a1a1a] rounded-md"></div>
+      `;
+      sourceElementList.appendChild(skeletonElement);
 
-    const sourceLoad = document.createElement("div");
-    sourceLoad.className = "absolute top-4 right-4";
-    sourceLoad.innerHTML = `<svg aria-hidden="true" class="w-4 h-4 text-[#f0f0f0] animate-spin dark:text-[#0d0d0d] fill-[#a0a0a0]" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
-            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
-        </svg>`;
+      const source = await getProvider({
+        romaji: anime.title.romaji,
+        english: anime.title.english,
+      });
 
-    sourceElement.appendChild(sourceLoad);
+      console.log(source);
 
-    sourceElementList.appendChild(sourceElement);
+      if (!source) return skeletonElement.remove();
 
-    const source = await getProvider({
-      romaji: anime.title.romaji,
-      english: anime.title.english,
-    });
+      source.hosters.map(async (source_hoster) => {
+        const extensionIndex = extensions.stream.findIndex(
+          (extension_hoster) => extension_hoster.name === source_hoster.label,
+        );
 
-    console.log(source);
+        if (extensionIndex === -1) return;
 
-    sourceLoad.remove();
+        const extension = extensions.stream[extensionIndex];
 
-    if (!source) return;
+        console.log(source_hoster);
 
-    source.hosters.map(async (source_hoster) => {
-      const extensionIndex = extensions.stream.findIndex(
-        (extension_hoster) => extension_hoster.name === source_hoster.label,
-      );
+        const source_episode = await getEpisode(source_hoster, episodes[index]);
+        console.log(source_episode);
+        skeletonElement.remove();
+        if (!source_episode) return;
 
-      if (extensionIndex === -1) return;
+        const hosterElement = document.createElement("div");
+        hosterElement.className =
+          "relative w-full h-28 p-4 outline-1 outline-[#1a1a1a] rounded-md cursor-pointer space-y-2";
 
-      const extension = extensions.stream[extensionIndex];
+        sourceElementList.appendChild(hosterElement);
 
-      console.log(source_hoster);
-
-      const source_episode = await getEpisode(source_hoster, episode);
-      console.log(source_episode);
-      if (!source_episode) return;
-
-      const hosterElement = document.createElement("div");
-      hosterElement.className =
-        "relative w-full h-28 p-4 outline-1 outline-[#1a1a1a] rounded-md cursor-pointer space-y-2";
-
-      const hosterTitle = document.createElement("div");
-      hosterTitle.className =
-        "flex items-center space-x-2 text-white font-bold";
-      hosterTitle.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-tv-icon lucide-tv"><path d="m17 2-5 5-5-5"/><rect width="20" height="15" x="2" y="7" rx="2"/></svg>
+        const hosterTitle = document.createElement("div");
+        hosterTitle.className =
+          "flex items-center space-x-2 text-white font-bold";
+        hosterTitle.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-tv-icon lucide-tv"><path d="m17 2-5 5-5-5"/><rect width="20" height="15" x="2" y="7" rx="2"/></svg>
       <span>${source_hoster.label}</span>`;
 
-      hosterElement.appendChild(hosterTitle);
+        hosterElement.appendChild(hosterTitle);
 
-      sourceElement.appendChild(hosterElement);
+        const extensionIcon = document.createElement("img");
+        extensionIcon.src = source_extension.icon;
+        extensionIcon.className = "absolute right-4 top-4 w-4 h-4";
+        extensionIcon.title = `Provided by ${source_extension.name}`;
 
-      const stream = await getMetadata(source_episode);
+        hosterElement.appendChild(extensionIcon);
 
-      if (!stream) return;
+        const stream = await getMetadata(source_episode);
 
-      console.log(stream);
+        if (!stream) return;
 
-      const hosterFileName = document.createElement("div");
-      hosterFileName.className = "text-[#a2a2a2] text-xs";
-      hosterFileName.textContent = stream.name;
+        console.log(stream);
 
-      hosterElement.appendChild(hosterFileName);
+        const hosterFileName = document.createElement("div");
+        hosterFileName.className = "text-[#a2a2a2] text-xs";
+        hosterFileName.textContent = stream.name;
 
-      const hosterSize = document.createElement("div");
-      hosterSize.className =
-        "absolute left-4 bottom-4 text-[#f0f0f0] text-xs py-1 m-0";
-      hosterSize.textContent = stream.size;
+        hosterElement.appendChild(hosterFileName);
 
-      hosterElement.appendChild(hosterSize);
+        const hosterSize = document.createElement("div");
+        hosterSize.className =
+          "absolute left-4 bottom-4 text-[#f0f0f0] text-xs py-1 m-0";
+        hosterSize.textContent = stream.size;
 
-      const hosterQuality = document.createElement("div");
-      hosterQuality.className =
-        "absolute right-4 bottom-4 px-2 py-1 text-xs text-black bg-[#FFBF00] rounded";
-      hosterQuality.textContent = stream.quality;
+        hosterElement.appendChild(hosterSize);
 
-      hosterElement.appendChild(hosterQuality);
+        const hosterQuality = document.createElement("div");
+        hosterQuality.className =
+          "absolute right-4 bottom-4 px-2 py-1 text-xs text-black bg-[#FFBF00] rounded";
+        hosterQuality.textContent = stream.quality;
 
-      hosterElement.addEventListener("click", () => {
-        router.navigate(
-          `/player?streamurl=${encodeURIComponent(stream.mp4)}&title=${encodeURIComponent(anime.title.romaji)}&episode=${JSON.stringify(episode)}&anilist_id=${anime.id}`,
-        );
+        hosterElement.appendChild(hosterQuality);
+
+        hosterElement.addEventListener("click", () => {
+          console.log(
+            `/player?streamurl=${encodeURIComponent(stream.mp4)}&title=${encodeURIComponent(anime.title.romaji)}&episode=${JSON.stringify(episodes[index])}&anilist_id=${anime.id}`,
+          );
+
+          router.navigate(
+            `/player?streamurl=${encodeURIComponent(stream.mp4)}&title=${encodeURIComponent(anime.title.romaji)}&episode=${JSON.stringify(episodes[index])}&anilist_id=${anime.id}`,
+          );
+        });
       });
     });
-  });
+  }
 
   return container;
 }
