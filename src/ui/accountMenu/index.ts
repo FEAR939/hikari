3de839d7b88d85cda57f678e8e17fcab22022980
FEAR939 <rogronin8877@gmail.com>
2025-09-27 00:44:24 +0200

    UI work

--- src/ui/accountMenu/index.ts
+++ src/ui/accountMenu/index.ts
@@ -5,19 +5,49 @@ import { accountAvatar } from "../accountAvatar/index";
 export function accountMenu() {
   const menu = document.createElement("div");
   menu.className =
-    "absolute z-5 top-10 right-0 h-fit w-64 p-4 hidden bg-[#0d0d0d] rounded-xl space-y-4";
+    "absolute z-5 top-10 right-0 h-fit w-64 p-2 opacity-0 bg-[#080808] rounded-2xl space-y-1 scale-75 transition-all ease-in-out duration-150";
+
+  let menustate = false;
 
   menu.toggleVisibility = () => {
-    menu.classList.toggle("hidden");
+    menustate = !menustate;
+
+    if (menustate) menu.classList.remove("hidden");
+    menu.offsetHeight;
+    menu.classList.toggle("opacity-0");
+    menu.classList.toggle("translate-y-2");
+    menu.classList.toggle("scale-75");
+
+    menu.addEventListener("transitionend", function handler() {
+      if (!menustate) menu.classList.add("hidden");
+      menu.removeEventListener("transitionend", handler);
+    });
   };
 
-  menu.appendChild(accountAvatar());
+  const avatar = accountAvatar();
+  avatar.classList.add("m-2", "mb-4");
+
+  menu.appendChild(avatar);
+
+  const settingsLink = document.createElement("div");
+  settingsLink.className =
+    "w-full px-2 py-1 rounded-md text-sm text-[#e0e0e0] flex items-center space-x-2 cursor-pointer";
+  settingsLink.innerHTML = `
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-settings-icon lucide-settings size-4"><path d="M9.671 4.136a2.34 2.34 0 0 1 4.659 0 2.34 2.34 0 0 0 3.319 1.915 2.34 2.34 0 0 1 2.33 4.033 2.34 2.34 0 0 0 0 3.831 2.34 2.34 0 0 1-2.33 4.033 2.34 2.34 0 0 0-3.319 1.915 2.34 2.34 0 0 1-4.659 0 2.34 2.34 0 0 0-3.32-1.915 2.34 2.34 0 0 1-2.33-4.033 2.34 2.34 0 0 0 0-3.831A2.34 2.34 0 0 1 6.35 6.051a2.34 2.34 0 0 0 3.319-1.915"/><circle cx="12" cy="12" r="3"/></svg>
+    <span>Settings</span>
+  `;
+
+  menu.appendChild(settingsLink);
+
+  settingsLink.addEventListener("click", () => {
+    router.navigate("/settings");
+  });
 
   let state = 0;
 
   const signButton = document.createElement("div");
   signButton.className =
-    "w-full px-2 py-1 rounded-md bg-white text-black cursor-pointer";
+    "w-full px-2 py-1 rounded-md text-sm text-[#e0e0e0] flex items-center space-x-2 cursor-pointer";
   signButton.textContent = "Signup | Signin";
 
   menu.appendChild(signButton);
@@ -44,7 +74,10 @@ export function accountMenu() {
       return;
     }
 
-    signButton.textContent = "Signout";
+    signButton.innerHTML = `
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-log-out-icon lucide-log-out size-4"><path d="m16 17 5-5-5-5"/><path d="M21 12H9"/><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"/></svg>
+      <span>Sign Out</span>
+    `;
     state = 1;
   }
 
