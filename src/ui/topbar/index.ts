dba4dd6c7e2140dfc2470b48f8ae1a06a51205ce
FEAR939 <rogronin8877@gmail.com>
2025-09-27 00:44:34 +0200

    moved settings into accountmenu

--- src/ui/topbar/index.ts
+++ src/ui/topbar/index.ts
@@ -18,17 +18,6 @@ export function topBar() {
     router.navigate("/search");
   });
 
-  const settings = document.createElement("div");
-  settings.className =
-    "flex items-center justify-center w-8 h-8 rounded-full cursor-pointer";
-  settings.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-settings-icon lucide-settings"><path d="M9.671 4.136a2.34 2.34 0 0 1 4.659 0 2.34 2.34 0 0 0 3.319 1.915 2.34 2.34 0 0 1 2.33 4.033 2.34 2.34 0 0 0 0 3.831 2.34 2.34 0 0 1-2.33 4.033 2.34 2.34 0 0 0-3.319 1.915 2.34 2.34 0 0 1-4.659 0 2.34 2.34 0 0 0-3.32-1.915 2.34 2.34 0 0 1-2.33-4.033 2.34 2.34 0 0 0 0-3.831A2.34 2.34 0 0 1 6.35 6.051a2.34 2.34 0 0 0 3.319-1.915"/><circle cx="12" cy="12" r="3"/></svg>`;
-
-  bar.appendChild(settings);
-
-  settings.addEventListener("click", () => {
-    router.navigate("/settings");
-  });
-
   const avatar = accountAvatar();
 
   const avatarMenu = accountMenu();
