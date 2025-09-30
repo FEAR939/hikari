e19f4ef220a2c063d3b0e79be1eb3c2ac23c9ddf
FEAR939 <rogronin8877@gmail.com>
2025-09-30 21:00:47 +0200

    improved UI and now shows username and email when logged in

--- src/ui/accountMenu/index.ts
+++ src/ui/accountMenu/index.ts
@@ -5,9 +5,14 @@ import { accountAvatar } from "../accountAvatar/index";
 export function accountMenu() {
   const menu = document.createElement("div");
   menu.className =
-    "absolute z-5 top-10 right-0 h-fit w-64 p-2 opacity-0 bg-[#080808] rounded-2xl space-y-1 scale-75 transition-all ease-in-out duration-150";
+    "absolute z-5 top-8 right-0 h-fit w-64 p-2 opacity-0 hidden bg-[#1a1a1a]/95 backdrop-blur-md rounded-lg ring-1 ring-gray-700/50 shadow-lg space-y-1 scale-75 transition-all ease-in-out duration-150";
+  menu.trigger = null;
 
   let menustate = false;
+
+  menu.registerTrigger = (node) => {
+    menu.trigger = node;
+  };
 
   menu.toggleVisibility = () => {
     menustate = !menustate;
@@ -24,14 +29,55 @@ export function accountMenu() {
     });
   };
 
-  const avatar = accountAvatar();
-  avatar.classList.add("m-2", "mb-4");
+  window.addEventListener("click", (e) => {
+    if (
+      !menustate ||
+      menu.contains(e.target) ||
+      menu.trigger.contains(e.target)
+    )
+      return;
 
-  menu.appendChild(avatar);
+    menu.toggleVisibility();
+  });
+
+  const accountArea = document.createElement("div");
+  accountArea.className = "flex space-x-2 p-1 mb-4 items-center";
+
+  const avatar = accountAvatar();
+
+  accountArea.appendChild(avatar);
+
+  const accountSide = document.createElement("div");
+  accountSide.className = "h-8 flex flex-col justify-around";
+
+  const usernickname = document.createElement("div");
+  usernickname.className = "text-sm inline-block leading-none";
+  usernickname.textContent = "Anonymous";
+
+  accountSide.appendChild(usernickname);
+
+  const useremail = document.createElement("div");
+  useremail.className = "text-xs text-gray-500 leading-none";
+  useremail.textContent = "Sign up for more features!";
+
+  accountSide.appendChild(useremail);
+
+  authService.subscribe((user) => {
+    if (user) {
+      usernickname.textContent = user.username;
+      useremail.textContent = user.email;
+    } else {
+      usernickname.textContent = "Anonymous";
+      useremail.textContent = "Sign up for more features!";
+    }
+  });
+
+  accountArea.appendChild(accountSide);
+  menu.appendChild(accountArea);
 
   const settingsLink = document.createElement("div");
   settingsLink.className =
-    "w-full px-2 py-1 rounded-md text-sm text-[#e0e0e0] flex items-center space-x-2 cursor-pointer";
+    "w-full px-2 py-1 rounded text-sm text-[#e0e0e0] flex items-center space-x-2 cursor-pointer hover:bg-[#fafafa]/15 hover:backdrop-blur-xl transition-all duration-100";
   settingsLink.innerHTML = `
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-settings-icon lucide-settings size-4"><path d="M9.671 4.136a2.34 2.34 0 0 1 4.659 0 2.34 2.34 0 0 0 3.319 1.915 2.34 2.34 0 0 1 2.33 4.033 2.34 2.34 0 0 0 0 3.831 2.34 2.34 0 0 1-2.33 4.033 2.34 2.34 0 0 0-3.319 1.915 2.34 2.34 0 0 1-4.659 0 2.34 2.34 0 0 0-3.32-1.915 2.34 2.34 0 0 1-2.33-4.033 2.34 2.34 0 0 0 0-3.831A2.34 2.34 0 0 1 6.35 6.051a2.34 2.34 0 0 0 3.319-1.915"/><circle cx="12" cy="12" r="3"/></svg>
     <span>Settings</span>
@@ -47,7 +93,7 @@ export function accountMenu() {
 
   const signButton = document.createElement("div");
   signButton.className =
-    "w-full px-2 py-1 rounded-md text-sm text-[#e0e0e0] flex items-center space-x-2 cursor-pointer";
+    "w-full px-2 py-1 rounded text-sm text-[#e0e0e0] flex items-center space-x-2 cursor-pointer hover:bg-[#fafafa]/15 hover:backdrop-blur-xl transition-all duration-100";
   signButton.textContent = "Signup | Signin";
 
   menu.appendChild(signButton);
