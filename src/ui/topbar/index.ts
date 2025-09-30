8103de1a3d5d2dd7986ddd506a3052bb9fb394b3
FEAR939 <rogronin8877@gmail.com>
2025-09-30 21:02:00 +0200

    made the bar bigger and added stuff for the menu to work properly

--- src/ui/topbar/index.ts
+++ src/ui/topbar/index.ts
@@ -5,12 +5,12 @@ import { accountMenu } from "../accountMenu/index";
 export function topBar() {
   const bar = document.createElement("div");
   bar.className =
-    "absolute top-0 left-0 right-0 h-12 w-full px-4 flex items-center justify-end space-x-2 bg-gradient-to-t from-transparent to-[#0d0d0d]";
+    "absolute top-0 left-0 right-0 h-16 w-full px-4 flex items-center justify-end space-x-4 bg-gradient-to-t from-transparent to-[#0d0d0d]";
 
   const search = document.createElement("div");
   search.className =
-    "flex items-center justify-center w-8 h-8 rounded-full cursor-pointer";
-  search.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-search-icon lucide-search"><path d="m21 21-4.34-4.34"/><circle cx="11" cy="11" r="8"/></svg>`;
+    "flex items-center justify-center size-5 rounded-full cursor-pointer";
+  search.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-search-icon lucide-search size-5"><path d="m21 21-4.34-4.34"/><circle cx="11" cy="11" r="8"/></svg>`;
 
   bar.appendChild(search);
 
@@ -23,6 +23,7 @@ export function topBar() {
   const avatarMenu = accountMenu();
 
   avatar.appendChild(avatarMenu);
+  avatarMenu.registerTrigger(avatar);
 
   avatar.addEventListener("click", (e) => {
     if (e.target === avatarMenu || avatarMenu.contains(e.target)) return;
