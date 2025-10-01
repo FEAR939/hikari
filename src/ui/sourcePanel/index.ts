952410aba4225446275cecc7ba398797f28e5203
FEAR939 <rogronin8877@gmail.com>
2025-10-01 19:57:18 +0200

    changed file selector UI and it now actually is the child of the panel...

--- src/ui/sourcePanel/index.ts
+++ src/ui/sourcePanel/index.ts
@@ -72,7 +72,7 @@ export function SourcePanel(anime, episodes, index) {
 
   const localFileWrapper = document.createElement("div");
   localFileWrapper.className =
-    "relative w-full px-4 py-4 grid place-items-center space-y-1 text-sm bg-[#080808] outline-1 outline-[#1a1a1a] rounded-md";
+    "relative w-full px-4 py-4 grid place-items-center space-y-1 text-sm bg-[#0d0d0d] outline-1 outline-dashed outline-[#1a1a1a] rounded-md";
   localFileWrapper.innerHTML = `
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-hard-drive-icon lucide-hard-drive size-8"><line x1="22" x2="2" y1="12" y2="12"/><path d="M5.45 5.11 2 12v6a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-6l-3.45-6.89A2 2 0 0 0 16.76 4H7.24a2 2 0 0 0-1.79 1.11z"/><line x1="6" x2="6.01" y1="16" y2="16"/><line x1="10" x2="10.01" y1="16" y2="16"/></svg>
     <span class="text-white">Drag & Drop <span class="text-gray-400">or</span></span>
@@ -86,7 +86,7 @@ export function SourcePanel(anime, episodes, index) {
 
   localFileWrapper.appendChild(localFileInput);
 
-  episodePicker.appendChild(localFileWrapper);
+  panel.appendChild(localFileWrapper);
 
   localFileInput.addEventListener("change", () => {
     const file = localFileInput.files[0];
