7acfc10836d71b2561509b4a875d952ebf84b8b2
FEAR939 <rogronin8877@gmail.com>
2025-10-04 03:47:49 +0200

    changed the episode layout

--- src/pages/anime/index.ts
+++ src/pages/anime/index.ts
@@ -316,7 +316,7 @@ export default async function Anime(query) {
   function episodeHandler() {
     const episodeList = document.createElement("div");
     episodeList.className =
-      "h-fit w-full grid grid-cols-1 md:grid-cols-2 gap-4";
+      "h-fit w-full grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-4";
 
     tabContent.appendChild(episodeList);
 
