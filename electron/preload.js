bc81c5099f207a96e3a008130dff8e83d5527757
FEAR939 <rogronin8877@gmail.com>
2025-10-04 03:19:17 +0200

    added local media handle

--- electron/preload.js
+++ electron/preload.js
@@ -3,4 +3,8 @@ const { contextBridge, ipcRenderer } = require("electron");
 contextBridge.exposeInMainWorld("electronAPI", {
   enterFullscreen: () => ipcRenderer.send("enter-fullscreen"),
   exitFullscreen: () => ipcRenderer.send("exit-fullscreen"),
+  getLocalMedia: (dirPath) => {
+    const files = ipcRenderer.invoke("get-local-media", dirPath);
+    return files;
+  },
 });
