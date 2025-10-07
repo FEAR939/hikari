366c67fca27b3f47c8e10604eb42c196282be360
FEAR939 <rogronin8877@gmail.com>
2025-10-07 22:47:42 +0200

    added loading indicator

--- src/pages/player/index.ts
+++ src/pages/player/index.ts
@@ -62,6 +62,12 @@ export default async function Player(query: PlayerQuery) {
 
   player.appendChild(video);
 
+  const loading_indicator = document.createElement("div");
+  loading_indicator.className = "absolute inset-0 size-16 m-auto animate-spin";
+  loading_indicator.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-loader-circle-icon lucide-loader-circle size-16"><path d="M21 12a9 9 0 1 1-6.219-8.56"/></svg>`;
+
+  player.appendChild(loading_indicator);
+
   const controls = document.createElement("div");
   controls.className =
     "absolute bottom-0 left-0 right-0 h-26 bg-gradient bg-gradient-to-t from-black to-transparent px-4 py-2 space-y-2 flex flex-col justify-around";
@@ -270,6 +276,7 @@ export default async function Player(query: PlayerQuery) {
 
   video.addEventListener("loadedmetadata", async () => {
     canPlay = true;
+    loading_indicator.remove();
     video.play();
     controls.classList.add("hidden");
     pageback.classList.add("hidden");
