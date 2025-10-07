4f1122d2dd81923937c87760baedb8ecd0687ca8
FEAR939 <rogronin8877@gmail.com>
2025-10-07 16:52:18 +0200

    added options and types; now has a relation mode

--- src/ui/card/index.ts
+++ src/ui/card/index.ts
@@ -1,6 +1,16 @@
 import { router } from "../../lib/router";
 
-export function Card(item) {
+interface CardOptions {
+  label?: boolean;
+  type?: CardType;
+}
+
+export enum CardType {
+  DEFAULT = 0,
+  RELATION = 1,
+}
+
+export function Card(item, options: CardOptions = {}) {
   const card = document.createElement("div");
   card.className = "h-fit w-36 md:w-48 shrink-0 cursor-pointer";
 
@@ -10,6 +20,15 @@ export function Card(item) {
   cardImage.loading = "lazy"; // Add lazy loading
 
   card.appendChild(cardImage);
+
+  if (options.type === CardType.RELATION && options.label) {
+    const cardLabel = document.createElement("div");
+    cardLabel.className = "text-sm mt-2 font-medium space-y-1";
+    cardLabel.innerHTML = `<div class="text-white line-clamp-2">${item.title.english || item.title.romaji}</div><div class="text-neutral-500 text-xs">${item.relationType}</div>`;
+
+    card.appendChild(cardLabel);
+  }
+
   card.addEventListener("click", () => {
     router.navigate(`/anime?id=${item.id}`);
   });
