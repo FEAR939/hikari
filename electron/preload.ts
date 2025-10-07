4c64074b2b370affa1aaffdeb53538c8c6cfa0a8
FEAR939 <rogronin8877@gmail.com>
2025-10-07 23:55:15 +0200

    added interface for getting app version

--- electron/preload.ts
+++ electron/preload.ts
@@ -1,4 +1,4 @@
-const { contextBridge, ipcRenderer } = require("electron");
+const { contextBridge, ipcRenderer, app } = require("electron");
 
 contextBridge.exposeInMainWorld("electronAPI", {
   enterFullscreen: () => ipcRenderer.send("enter-fullscreen"),
@@ -6,5 +6,9 @@ contextBridge.exposeInMainWorld("electronAPI", {
   getLocalMedia: (dirPath) => {
     const files = ipcRenderer.invoke("get-local-media", dirPath);
     return files;
+  },
+  getAppVersion: async () => {
+    const version = await ipcRenderer.invoke("get-app-version");
+    return version;
   },
 });
