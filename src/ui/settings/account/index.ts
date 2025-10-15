fac0e851fed1f6b0c1c59f229da5bb3d99eb170a
FEAR939 <rogronin8877@gmail.com>
2025-10-15 20:08:51 +0200

    the avatar is now bigger

--- src/ui/settings/account/index.ts
+++ src/ui/settings/account/index.ts
@@ -27,10 +27,10 @@ export default function AccountSettings() {
   }
 
   const avatarUpload = document.createElement("div");
-  avatarUpload.className = "group relative size-16 rounded-full cursor-pointer";
+  avatarUpload.className = "group relative size-20 rounded-full cursor-pointer";
 
   const avatar = accountAvatar();
-  avatar.className = "size-16";
+  avatar.className = "size-20";
   avatarUpload.appendChild(avatar);
 
   const avatarFileInput = document.createElement("input");
@@ -58,7 +58,7 @@ export default function AccountSettings() {
 
   const avatarEdit = document.createElement("div");
   avatarEdit.className =
-    "absolute right-0 bottom-0 bg-white text-black rounded-full size-5 grid place-items-center opacity-0 group-hover:opacity-100 transition-opacity duration-300";
+    "absolute right-0 bottom-0 bg-white text-black rounded-full size-6 grid place-items-center opacity-0 group-hover:opacity-100 transition-opacity duration-300";
   avatarEdit.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-pencil-icon lucide-pencil size-3"><path d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z"/><path d="m15 5 4 4"/></svg>`;
 
   avatarUpload.appendChild(avatarEdit);
