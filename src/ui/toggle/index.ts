730a6db53341e508757ae981c1f65028815ed1b6
FEAR939 <rogronin8877@gmail.com>
2025-10-15 20:08:20 +0200

    is now green when enabled

--- src/ui/toggle/index.ts
+++ src/ui/toggle/index.ts
@@ -1,28 +1,47 @@
 export function Toggle(callback) {
   const toggle = document.createElement("div");
-  toggle.className = "h-4 min-w-8 rounded-full bg-neutral-800";
+  toggle.className =
+    "h-4 min-w-8 w-8 max-w-8 rounded-full transition-background duration-150 bg-neutral-800";
 
   const toggleNob = document.createElement("div");
   toggleNob.className =
-    "w-4 h-4 rounded-full bg-white transition-transform duration-300";
+    "w-4 h-4 rounded-full bg-white transition-transform duration-150 translate-x-0";
 
   toggle.appendChild(toggleNob);
 
   let toggleState = false;
 
+  function updateState() {
+    if (toggleState) {
+      toggle.className = toggle.className.replace(
+        "bg-neutral-800",
+        "bg-green-400",
+      );
+      toggleNob.className = toggleNob.className.replace(
+        "translate-x-0",
+        "translate-x-full",
+      );
+    } else {
+      toggle.className = toggle.className.replace(
+        "bg-green-400",
+        "bg-neutral-800",
+      );
+      toggleNob.className = toggleNob.className.replace(
+        "translate-x-full",
+        "translate-x-0",
+      );
+    }
+  }
+
   toggle.addEventListener("click", () => {
     toggleState = !toggleState;
-    toggleNob.style.transform = toggleState
-      ? "translateX(100%)"
-      : "translateX(0)";
+    updateState();
     callback(toggleState);
   });
 
   toggle.setState = (state: boolean) => {
     toggleState = state;
-    toggleNob.style.transform = toggleState
-      ? "translateX(100%)"
-      : "translateX(0)";
+    updateState();
   };
 
   toggle.getState = () => toggleState;
