07e3ecd4301c357e2602a04af77fadb61a9c699a
FEAR939 <rogronin8877@gmail.com>
2025-10-20 00:12:11 +0200

    added types

--- src/lib/anime2you/index.ts
+++ src/lib/anime2you/index.ts
@@ -1,5 +1,14 @@
 import { cache } from "../../services/cache";
 
+export interface NewsItem {
+  image: string | null;
+  title: string | null;
+  link: string | null;
+  date: string | null;
+}
+
+export type NewsResponse = NewsItem[];
+
 export class Client {
   baseUrl: string;
 
@@ -7,7 +16,7 @@ export class Client {
     this.baseUrl = "https://www.anime2you.de/news/";
   }
 
-  async getNews() {
+  async getNews(): Promise<NewsResponse> {
     if (cache.get("news")) {
       return cache.get("news");
     }
@@ -23,19 +32,21 @@ export class Client {
     return newsItems;
   }
 
-  scrapeFromPage(html: string) {
+  scrapeFromPage(html: string): NewsResponse {
     const parser = new DOMParser();
     const doc = parser.parseFromString(html, "text/html");
 
     const newsItems = doc.querySelectorAll(".td-cpt-post");
 
     const news = Array.from(newsItems).map((item) => {
-      const image = item
-        .querySelector(".entry-thumb")
-        ?.getAttribute("data-img-url");
-      const title = item.querySelector(".entry-title a")?.textContent;
-      const link = item.querySelector(".entry-title a")?.getAttribute("href");
-      const date = item.querySelector(".entry-date")?.getAttribute("datetime");
+      const image =
+        item.querySelector(".entry-thumb")?.getAttribute("data-img-url") ||
+        null;
+      const title = item.querySelector(".entry-title a")?.textContent || null;
+      const link =
+        item.querySelector(".entry-title a")?.getAttribute("href") || null;
+      const date =
+        item.querySelector(".entry-date")?.getAttribute("datetime") || null;
 
       return { image, title, link, date };
     });
