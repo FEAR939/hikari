859b5d9519a961e02c21c052fb6c7e90df80dc09
FEAR939 <rogronin8877@gmail.com>
2025-10-23 06:33:56 +0200

    change: add class merge for slider

--- src/ui/card/index.tsx
+++ src/ui/card/index.tsx
@@ -1,5 +1,6 @@
 import { router } from "../../lib/router";
 import { h } from "../../lib/jsx/runtime";
+import { twMerge } from "tailwind-merge";
 
 interface CardOptions {
   label?: boolean;
@@ -19,9 +20,11 @@ export enum CardSize {
 
 export function Card({
   item,
+  className = "",
   options = {},
 }: {
   item: any;
+  className?: string;
   options?: CardOptions;
 }) {
   if (!options.size) options.size = CardSize.MANUAL;
@@ -29,29 +32,30 @@ export function Card({
   if (!options.label) options.label = false;
 
   return (
-    <div>
-      <div
-        class={`h-fit ${options.size === CardSize.MANUAL ? "w-36 md:w-48" : "w-full"} shrink-0 cursor-pointer`}
-        onclick={() => router.navigate(`/anime?id=${item.id}`)}
-      >
-        <img
-          src={item.attributes.posterImage.original}
-          class="w-full aspect-[5/7] object-cover rounded-lg"
-          loading="lazy"
-        />
-        {options.label && (
-          <div class="text-sm mt-2 font-medium space-y-1">
-            <div class="text-white line-clamp-2">
-              {item.attributes.titles.en ||
-                item.attributes.titles.en_us ||
-                item.attributes.titles.en_jp}
-            </div>
-            {item.relationType && (
-              <div class="text-neutral-500 text-xs">{item.relationType}</div>
-            )}
+    <div
+      class={twMerge(
+        className,
+        `h-fit ${options.size === CardSize.MANUAL ? "w-36 md:w-48" : "w-full"} cursor-pointer shrink-0`,
+      )}
+      onclick={() => router.navigate(`/anime?id=${item.id}`)}
+    >
+      <img
+        src={item.attributes.posterImage.original}
+        class="w-full aspect-[5/7] object-cover rounded-lg"
+        loading="lazy"
+      />
+      {options.label && (
+        <div class="text-sm mt-2 font-medium space-y-1">
+          <div class="text-white line-clamp-2">
+            {item.attributes.titles.en ||
+              item.attributes.titles.en_us ||
+              item.attributes.titles.en_jp}
           </div>
-        )}
-      </div>
+          {item.relationType && (
+            <div class="text-neutral-500 text-xs">{item.relationType}</div>
+          )}
+        </div>
+      )}
     </div>
   );
 }
