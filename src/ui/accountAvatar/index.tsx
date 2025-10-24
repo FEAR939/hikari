5fa443109b140ddfa769550433351dd32ba058f7
FEAR939 <rogronin8877@gmail.com>
2025-10-24 02:19:04 +0200

    fix: avatar fallback now works

--- src/ui/accountAvatar/index.tsx
+++ src/ui/accountAvatar/index.tsx
@@ -35,33 +35,34 @@ export function AccountAvatar() {
                   class="absolute inset-0 w-full h-full object-cover object-center rounded-full"
                 />
               );
-            } else {
+            } else if (value.avatar === null) {
+              console.log(value.avatar);
               return (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  class="lucide lucide-user-icon lucide-user"
-                >
-                  <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
-                  <circle cx="12" cy="7" r="4" />
-                </svg>
+                <div class="h-full w-full flex items-center justify-center bg-pink-500 rounded-full">
+                  {(value?.username)
+                    .replaceAll(" ", "")
+                    .substring(0, 2)
+                    .toUpperCase()}
+                </div>
               );
             }
           } else {
             return (
-              <div class="h-full w-full flex items-center justify-center bg-gray-300 rounded-full">
-                {value?.username
-                  .replaceAll(" ", "")
-                  .substring(0, 2)
-                  .toUpperCase()}
-              </div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="lucide lucide-user-icon lucide-user"
+              >
+                <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
+                <circle cx="12" cy="7" r="4" />
+              </svg>
             );
           }
         })}
