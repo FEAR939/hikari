7dca44b19bf8cd4971d8f29077f8d5a028f2f66d
FEAR939 <rogronin8877@gmail.com>
2025-10-28 00:33:39 +0100

    change: removed back button

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -11,28 +11,7 @@ export default async function Search(query) {
 
   const page = (
     <div class="relative h-full w-full p-4 pt-12 space-y-4 overflow-y-scroll">
-      <div
-        class="absolute z-10 top-2 left-4 size-8 flex items-center justify-center cursor-pointer"
-        onclick={() => router.navigate("/")}
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          class="lucide lucide-arrow-left-to-line-icon lucide-arrow-left-to-line"
-        >
-          <path d="M3 19V5" />
-          <path d="m13 6-6 6 6 6" />
-          <path d="M7 12h14" />
-        </svg>
-      </div>
-      <div class="rounded-lg border-1 border-neutral-700/50 bg-neutral-950 h-10 w-64 px-3 flex items-center overflow-hidden">
+      <div class="rounded-lg border border-neutral-700/50 bg-neutral-950 h-10 w-64 px-3 flex items-center overflow-hidden">
         <div class="h-full flex items-center shrink-0 text-neutral-700">
           <svg
             xmlns="http://www.w3.org/2000/svg"
