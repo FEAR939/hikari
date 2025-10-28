8244e43913142f7de29e2f90495a5e90f2e5376e
FEAR939 <rogronin8877@gmail.com>
2025-10-28 17:55:32 +0100

    change: move slightly to the right

--- src/ui/accountMenu/index.tsx
+++ src/ui/accountMenu/index.tsx
@@ -10,8 +10,8 @@ export function accountMenu() {
     email: string;
   }>(null);
   const menu = (
-    <div class="absolute z-5 top-8 right-0 h-fit w-64 p-1 opacity-0 hidden bg-neutral-900 backdrop-blur-md rounded-lg ring-1 ring-neutral-700/50 shadow-lg space-y-1 scale-75 transition-all ease-in-out duration-150">
-      <div class="flex space-x-2 p-1 pb-2 mb-2 items-center border-b-1 border-neutral-700/50">
+    <div class="absolute z-5 top-8 right-0 translate-x-1/2 h-fit w-64 p-1 opacity-0 hidden bg-neutral-900 backdrop-blur-md rounded-lg ring-1 ring-neutral-700/50 shadow-lg space-y-1 scale-75 transition-all ease-in-out duration-150">
+      <div class="flex space-x-2 p-1 pb-2 mb-2 items-center border-b border-neutral-700/50">
         <AccountAvatar />
         {bind([user, setUser, subscribeUser], (value) => (
           <div class="h-8 flex flex-col justify-around">
