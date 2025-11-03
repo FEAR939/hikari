af64cd020c4885af8c5f19f178fad047969ca3b5
FEAR939 <rogronin8877@gmail.com>
2025-11-03 18:25:05 +0100

    change: fixed total episode count not being updated and the dir list is now a table with additional data

--- src/pages/files/index.tsx
+++ src/pages/files/index.tsx
@@ -7,8 +7,8 @@ export default async function Files(query) {
 
   const [baseDir, setBaseDir, subscribeBaseDir] = createSignal<string[]>([]);
   const [episodeCount, setEpisodeCount, subscribeEpisodeCount] =
-    createSignal(0);
-  const [dirSize, setDirSize, subscribeDirSize] = createSignal(0);
+    createSignal<number>(0);
+  const [dirSize, setDirSize, subscribeDirSize] = createSignal<number>(0);
 
   const page = (
     <div class="h-full w-full p-4 pt-12 space-y-4 overflow-y-scroll">
@@ -36,11 +36,18 @@ export default async function Files(query) {
         </div>
       </div>
       {bind([baseDir, setBaseDir, subscribeBaseDir], (value) => (
-        <div>
+        <div class="border border-neutral-900 rounded-lg">
+          <div class="px-6 py-3 grid grid-cols-3 text-sm text-neutral-500 border-b border-neutral-900">
+            <div>Title</div>
+            <div>Episodes</div>
+            <div>Size</div>
+          </div>
           {value.length > 0 ? (
             value.map((dir) => (
-              <div class="text-sm px-3 py-1.5 hover:bg-neutral-900 rounded-md">
-                {dir}
+              <div class="text-sm px-6 py-3 grid grid-cols-3 hover:bg-neutral-800 border-b border-neutral-900">
+                <div>{dir}</div>
+                <EntryEpisodeCount basePath={basePath} dir={dir} />
+                <EntryDirSize basePath={basePath} dir={dir} />
               </div>
             ))
           ) : (
@@ -53,29 +60,56 @@ export default async function Files(query) {
 
   router.container!.appendChild(page);
 
+  subscribeBaseDir(async () => {
+    const episodeCountResult = await getEpisodeCount(basePath || "", baseDir());
+    setEpisodeCount(episodeCountResult);
+  });
+
   const baseDirResult =
     (await window.electronAPI?.getDir(basePath || "")) || [];
   setBaseDir(baseDirResult);
 
-  subscribeBaseDir(async () => {
-    const episodeCountResult = (
-      await Promise.all(
-        baseDir().map(async (dir) => {
-          const path = `${basePath}${dir}`;
-          const files = await window.electronAPI?.getDir(path);
-          return (
-            files?.filter(
-              (file) => file.endsWith(".mp4") || file.endsWith(".mkv"),
-            ).length || 0
-          );
-        }),
-      )
-    ).reduce((acc: number, curr: number) => acc + curr, 0);
-    setEpisodeCount(episodeCountResult);
-  });
-
-  const dirSizeResult = await window.electronAPI?.getDirSize(basePath || "");
+  const dirSizeResult =
+    (await window.electronAPI?.getDirSize(basePath || "")) || 0;
   setDirSize(dirSizeResult);
+}
+
+function EntryEpisodeCount({ basePath, dir }) {
+  const [count, setCount, subscribeCount] = createSignal(0);
+  new Promise(async () => {
+    const episodeCountResult = await getEpisodeCount(basePath || "", [dir]);
+    setCount(episodeCountResult);
+  });
+  return bind([count, setCount, subscribeCount], (value) => <div>{value}</div>);
+}
+
+function EntryDirSize({ basePath, dir }) {
+  const [size, setSize, subscribeSize] = createSignal(0);
+  new Promise(async () => {
+    const path = `${basePath}${dir}`;
+    const dirSizeResult = await window.electronAPI?.getDirSize(path);
+    setSize(dirSizeResult || 0);
+  });
+  return bind([size, setSize, subscribeSize], (value) => (
+    <div>{formatBytes(value)}</div>
+  ));
+}
+
+async function getEpisodeCount(basePath: string, baseDir: string[]) {
+  return (
+    await Promise.all(
+      baseDir.map(async (dir) => {
+        const path = `${basePath}${dir}`;
+
+        const files = await window.electronAPI?.getDir(path);
+        return (
+          files?.filter(
+            (file) => file.endsWith(".mp4") || file.endsWith(".mkv"),
+          ).length || 0
+        );
+      }),
+    )
+  ).reduce((acc: number, curr: number) => acc + curr, 0);
 }
 
 function formatBytes(bytes: number, decimals: number = 2) {
