fa0ef984fcaa23b85b412d5043e87e6421d0610c
FEAR939 <rogronin8877@gmail.com>
2025-11-09 02:50:24 +0100

    fix: custom paths

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
@@ -7,6 +8,11 @@ export default defineConfig(({ mode }) => {
   return {
     plugins: [tailwindcss()],
     resolve: {
+      alias: {
+        "@ui": path.resolve(__dirname, "src/ui"),
+        "@lib": path.resolve(__dirname, "src/lib"),
+        "@services": path.resolve(__dirname, "src/services"),
+      },
       extensions: [".js", ".ts", ".jsx", ".tsx"],
     },
     base: "./",
