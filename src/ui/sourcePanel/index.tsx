fefc835e7c8f14099b0e2b8ca457d658dff8adb4
FEAR939 <rogronin8877@gmail.com>
2025-11-11 19:35:36 +0100

    change: moved action buttons into a submenu

--- src/ui/sourcePanel/index.tsx
+++ src/ui/sourcePanel/index.tsx
@@ -6,6 +6,7 @@ import { NumberInput } from "../numberInput";
 import { cache } from "../../services/cache";
 import { kitsu, KitsuAnime, KitsuEpisode } from "../../lib/kitsu";
 import { Tooltip } from "../Tooltip";
+import { Toggle } from "@ui/toggle";
 
 interface Episode {
   episode: string;
@@ -517,6 +518,8 @@ export function SourcePanel({
   });
 
   let panel: HTMLDivElement;
+  const [settingsState, setSettingsState, subscribeSettingsState] =
+    createSignal(false);
 
   const container = (
     <div class="fixed inset-0 p-4 pt-12 flex items-center justify-center backdrop-brightness-50 backdrop-blur-md transition-all duration-150 ease-in-out opacity-0">
@@ -604,75 +607,79 @@ export function SourcePanel({
           })()}
         </div>
 
-        {/* Button Row */}
-        <div class="flex space-x-2">
-          <Tooltip content="Create Local Folder">
-            <div
-              class="size-8 grid place-items-center outline-1 outline-[#1a1a1a] rounded-md cursor-pointer"
-              onClick={async () => {
-                const local_media_path = localStorage.getItem(
-                  "app_local_media_path",
-                );
-                if (!local_media_path) {
-                  console.warn("Local media path not found");
-                  return;
-                }
-                const animeTitles = [
-                  await sanitizeTitle(anime.attributes.titles.en || ""),
-                  await sanitizeTitle(anime.attributes.titles.en_us || ""),
-                  await sanitizeTitle(anime.attributes.titles.en_jp || ""),
-                  await sanitizeTitle(anime.attributes.titles.en_cn || ""),
-                ].filter((title) => title !== "");
-
-                window.electronAPI?.createLocalMediaDir(
-                  `${local_media_path}${animeTitles[0]}`,
-                );
-              }}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                class="lucide lucide-folder-cog size-5"
-              >
-                <path d="M10.3 20H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h3.98a2 2 0 0 1 1.69.9l.66 1.2A2 2 0 0 0 12 6h8a2 2 0 0 1 2 2v3.3" />
-                <path d="m14.305 19.53.923-.382" />
-                <path d="m15.228 16.852-.923-.383" />
-                <path d="m16.852 15.228-.383-.923" />
-                <path d="m16.852 20.772-.383.924" />
-                <path d="m19.148 15.228.383-.923" />
-                <path d="m19.53 21.696-.382-.924" />
-                <path d="m20.772 16.852.924-.383" />
-                <path d="m20.772 19.148.924.383" />
-                <circle cx="18" cy="18" r="3" />
-              </svg>
-            </div>
-          </Tooltip>
-
-          {/* Auto Select Toggle */}
-          <Tooltip content="Selection Mode">
-            {bind(
-              [autoSelect, setAutoSelect, subscribeAutoSelect],
-              (isAuto) => (
-                <div
-                  class={
-                    isAuto
-                      ? "px-2 py-1 bg-white text-black rounded-md cursor-pointer"
-                      : "px-2 py-1 bg-black text-white outline-1 outline-[#1a1a1a] rounded-md cursor-pointer"
+        {/* Settings */}
+        <div class="relative size-5 ml-auto">
+          <svg
+            width="100%"
+            height="100%"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            class="size-5 cursor-pointer"
+            onClick={() => setSettingsState(!settingsState())}
+          >
+            <path
+              d="M12.0005 15C13.6573 15 15.0005 13.6569 15.0005 12C15.0005 10.3431 13.6573 9 12.0005 9C10.3436 9 9.00049 10.3431 9.00049 12C9.00049 13.6569 10.3436 15 12.0005 15Z"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              d="M9.28957 19.3711L9.87402 20.6856C10.0478 21.0768 10.3313 21.4093 10.6902 21.6426C11.0492 21.8759 11.4681 22.0001 11.8962 22C12.3244 22.0001 12.7433 21.8759 13.1022 21.6426C13.4612 21.4093 13.7447 21.0768 13.9185 20.6856L14.5029 19.3711C14.711 18.9047 15.0609 18.5159 15.5029 18.26C15.9477 18.0034 16.4622 17.8941 16.9729 17.9478L18.4029 18.1C18.8286 18.145 19.2582 18.0656 19.6396 17.8713C20.021 17.6771 20.3379 17.3763 20.5518 17.0056C20.766 16.635 20.868 16.2103 20.8455 15.7829C20.823 15.3555 20.677 14.9438 20.4251 14.5978L19.5785 13.4344C19.277 13.0171 19.1159 12.5148 19.1185 12C19.1184 11.4866 19.281 10.9864 19.5829 10.5711L20.4296 9.40778C20.6814 9.06175 20.8275 8.65007 20.85 8.22267C20.8725 7.79528 20.7704 7.37054 20.5562 7C20.3423 6.62923 20.0255 6.32849 19.644 6.13423C19.2626 5.93997 18.833 5.86053 18.4074 5.90556L16.9774 6.05778C16.4667 6.11141 15.9521 6.00212 15.5074 5.74556C15.0645 5.48825 14.7144 5.09736 14.5074 4.62889L13.9185 3.31444C13.7447 2.92317 13.4612 2.59072 13.1022 2.3574C12.7433 2.12408 12.3244 1.99993 11.8962 2C11.4681 1.99993 11.0492 2.12408 10.6902 2.3574C10.3313 2.59072 10.0478 2.92317 9.87402 3.31444L9.28957 4.62889C9.0825 5.09736 8.73245 5.48825 8.28957 5.74556C7.84479 6.00212 7.33024 6.11141 6.81957 6.05778L5.38513 5.90556C4.95946 5.86053 4.52987 5.93997 4.14844 6.13423C3.76702 6.32849 3.45014 6.62923 3.23624 7C3.02206 7.37054 2.92002 7.79528 2.94251 8.22267C2.96499 8.65007 3.11103 9.06175 3.36291 9.40778L4.20957 10.5711C4.51151 10.9864 4.67411 11.4866 4.67402 12C4.67411 12.5134 4.51151 13.0137 4.20957 13.4289L3.36291 14.5922C3.11103 14.9382 2.96499 15.3499 2.94251 15.7773C2.92002 16.2047 3.02206 16.6295 3.23624 17C3.45036 17.3706 3.76727 17.6712 4.14864 17.8654C4.53001 18.0596 4.95949 18.1392 5.38513 18.0944L6.81513 17.9422C7.3258 17.8886 7.84034 17.9979 8.28513 18.2544C8.72966 18.511 9.08134 18.902 9.28957 19.3711Z"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+          <div
+            class="absolute z-30 top-6 right-0 h-fit w-64 p-2 bg-neutral-900 border border-neutral-800 rounded-lg space-y-2 hidden"
+            ref={(el: HTMLElement) => {
+              subscribeSettingsState(() => {
+                settingsState()
+                  ? el.classList.remove("hidden")
+                  : el.classList.add("hidden");
+              });
+            }}
+          >
+            <div class="h-fit w-full flex items-center justify-between">
+              <span class="text-sm">Create local directory</span>
+              <div
+                class="py-0.5 px-2 bg-neutral-200 hover:bg-neutral-400 text-sm text-black rounded cursor-pointer transition-colors duration-150"
+                onClick={async () => {
+                  const local_media_path = localStorage.getItem(
+                    "app_local_media_path",
+                  );
+                  if (!local_media_path) {
+                    console.warn("Local media path not found");
+                    return;
                   }
-                  onClick={() => setAutoSelect(!isAuto)}
-                >
-                  AUTO
-                </div>
-              ),
-            )}
-          </Tooltip>
+                  const animeTitles = [
+                    await sanitizeTitle(anime.attributes.titles.en || ""),
+                    await sanitizeTitle(anime.attributes.titles.en_us || ""),
+                    await sanitizeTitle(anime.attributes.titles.en_jp || ""),
+                    await sanitizeTitle(anime.attributes.titles.en_cn || ""),
+                  ].filter((title) => title !== "");
+
+                  window.electronAPI?.createLocalMediaDir(
+                    `${local_media_path}${animeTitles[0]}`,
+                  );
+                }}
+              >
+                Create
+              </div>
+            </div>
+            <div class="h-fit w-full flex items-center justify-between">
+              <span class="text-sm">Auto Source Selection</span>
+              <Toggle
+                initial={autoSelect()}
+                callback={(state: boolean) => {
+                  setAutoSelect(state);
+                }}
+              ></Toggle>
+            </div>
+          </div>
         </div>
 
         {/* Insert Space for Timer */}
