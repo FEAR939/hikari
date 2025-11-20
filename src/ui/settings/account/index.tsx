f4ed1c4fc8605cf6a22fec8f3c4f6b2aa4c4ba24
FEAR939 <rogronin8877@gmail.com>
2025-11-20 22:58:33 +0100

    change: replaced icon

--- src/ui/settings/account/index.tsx
+++ src/ui/settings/account/index.tsx
@@ -50,19 +50,20 @@ export default function AccountSettings() {
                 <AccountAvatar className="size-20" />
                 <div class="absolute right-0 bottom-0 bg-white text-black rounded-full size-6 grid place-items-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
                   <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
+                    width="100%"
+                    height="100%"
                     viewBox="0 0 24 24"
                     fill="currentColor"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    class="lucide lucide-pencil-icon lucide-pencil size-3"
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="size-3"
                   >
-                    <path d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z" />
-                    <path d="m15 5 4 4" />
+                    <path
+                      d="M2.87601 18.1156C2.92195 17.7021 2.94493 17.4954 3.00748 17.3022C3.06298 17.1307 3.1414 16.9676 3.24061 16.8171C3.35242 16.6475 3.49952 16.5005 3.7937 16.2063L17 3C18.1046 1.89543 19.8954 1.89543 21 3C22.1046 4.10457 22.1046 5.89543 21 7L7.7937 20.2063C7.49951 20.5005 7.35242 20.6475 7.18286 20.7594C7.03242 20.8586 6.86926 20.937 6.69782 20.9925C6.50457 21.055 6.29783 21.078 5.88434 21.124L2.49997 21.5L2.87601 18.1156Z"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
                   </svg>
                 </div>
               </div>
