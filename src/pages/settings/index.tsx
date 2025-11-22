00d0d3ae42b4b49eadfab296b6c7bfb773887177
FEAR939 <rogronin8877@gmail.com>
2025-11-22 01:15:09 +0100

    enh: bigger icons and more spacing

--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -15,7 +15,7 @@ const settingsTree = [
         viewBox="0 0 24 24"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
-        class="size-4"
+        class="size-5"
       >
         <path
           d="M5.3163 19.4384C5.92462 18.0052 7.34492 17 9 17H15C16.6551 17 18.0754 18.0052 18.6837 19.4384M16 9.5C16 11.7091 14.2091 13.5 12 13.5C9.79086 13.5 8 11.7091 8 9.5C8 7.29086 9.79086 5.5 12 5.5C14.2091 5.5 16 7.29086 16 9.5ZM22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12Z"
@@ -38,10 +38,10 @@ const settingsTree = [
         viewBox="0 0 24 24"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
-        class="size-4"
+        class="size-5"
       >
         <path
-          d="M15.0505 9H5.5C4.11929 9 3 7.88071 3 6.5C3 5.11929 4.11929 4 5.5 4H15.0505M8.94949 20H18.5C19.8807 20 21 18.8807 21 17.5C21 16.1193 19.8807 15 18.5 15H8.94949M3 17.5C3 19.433 4.567 21 6.5 21C8.433 21 10 19.433 10 17.5C10 15.567 8.433 14 6.5 14C4.567 14 3 15.567 3 17.5ZM21 6.5C21 8.433 19.433 10 17.5 10C15.567 10 14 8.433 14 6.5C14 4.567 15.567 3 17.5 3C19.433 3 21 4.567 21 6.5Z"
+          d="M5 21L5 15M5 15C6.10457 15 7 14.1046 7 13C7 11.8954 6.10457 11 5 11C3.89543 11 3 11.8954 3 13C3 14.1046 3.89543 15 5 15ZM5 7V3M12 21V15M12 7V3M12 7C10.8954 7 10 7.89543 10 9C10 10.1046 10.8954 11 12 11C13.1046 11 14 10.1046 14 9C14 7.89543 13.1046 7 12 7ZM19 21V17M19 17C20.1046 17 21 16.1046 21 15C21 13.8954 20.1046 13 19 13C17.8954 13 17 13.8954 17 15C17 16.1046 17.8954 17 19 17ZM19 9V3"
           stroke="currentColor"
           stroke-width="2"
           stroke-linecap="round"
@@ -60,7 +60,7 @@ const settingsTree = [
         viewBox="0 0 24 24"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
-        class="size-4"
+        class="size-5"
       >
         <path
           d="M7.5 4.5C7.5 3.11929 8.61929 2 10 2C11.3807 2 12.5 3.11929 12.5 4.5V6H13.5C14.8978 6 15.5967 6 16.1481 6.22836C16.8831 6.53284 17.4672 7.11687 17.7716 7.85195C18 8.40326 18 9.10218 18 10.5H19.5C20.8807 10.5 22 11.6193 22 13C22 14.3807 20.8807 15.5 19.5 15.5H18V17.2C18 18.8802 18 19.7202 17.673 20.362C17.3854 20.9265 16.9265 21.3854 16.362 21.673C15.7202 22 14.8802 22 13.2 22H12.5V20.25C12.5 19.0074 11.4926 18 10.25 18C9.00736 18 8 19.0074 8 20.25V22H6.8C5.11984 22 4.27976 22 3.63803 21.673C3.07354 21.3854 2.6146 20.9265 2.32698 20.362C2 19.7202 2 18.8802 2 17.2V15.5H3.5C4.88071 15.5 6 14.3807 6 13C6 11.6193 4.88071 10.5 3.5 10.5H2C2 9.10218 2 8.40326 2.22836 7.85195C2.53284 7.11687 3.11687 6.53284 3.85195 6.22836C4.40326 6 5.10218 6 6.5 6H7.5V4.5Z"
@@ -82,7 +82,7 @@ const settingsTree = [
         viewBox="0 0 24 24"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
-        class="size-4"
+        class="size-5"
       >
         <path
           d="M15.5 15L18.5 12L15.5 9M8.5 9L5.5 12L8.5 15M13 7L11 17M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12Z"
@@ -129,17 +129,17 @@ export default async function Settings(query) {
         </div>
         <div class="h-full w-xs p-6 space-y-2 border-r border-white/10">
           <div class="text-xl">Settings</div>
-          <div class="space-y-2">
+          <div class="space-y-4">
             {settingsTree.map((setting, index: number) =>
               bind(
                 [currSettings, setCurrSettings, subscribeCurrSettings],
                 (value) => (
                   <div
-                    class={`flex items-center gap-1 w-full ${value === index ? "text-neutral-200" : "text-neutral-500"} hover:text-neutral-200 transition-colors duration-150 cursor-pointer`}
+                    class={`flex items-center gap-2 w-full ${value === index ? "text-neutral-200" : "text-neutral-500"} hover:text-neutral-200 transition-colors duration-150 cursor-pointer`}
                     onClick={() => setCurrSettings(index)}
                   >
                     {setting.icon()}
-                    {setting.label}
+                    <div class="h-3 leading-none">{setting.label}</div>
                   </div>
                 ),
               ),
