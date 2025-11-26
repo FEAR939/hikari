c41b8a6cc91dd8df367769691b6c72e5c8fb25a6
FEAR939 <rogronin8877@gmail.com>
2025-11-26 20:33:16 +0100

    feat: rssclient class

---
+++ src/lib/rss/index.ts
@@ -0,0 +1,29 @@
+export default class RSSClient {
+  constructor(url: string) {
+    this.url = url;
+  }
+
+  async getRSSFeed() {
+    const response = await fetch(this.url);
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch RSS feed: ${response.status}`);
+    }
+
+    const data = await response.text();
+
+    const parser = new DOMParser();
+    const xmlDoc = parser.parseFromString(data, "text/xml");
+
+    const items = xmlDoc.getElementsByTagName("item");
+
+    const feed = Array.from(items).map((item) => ({
+      title: item.getElementsByTagName("title")[0].textContent,
+      link: item.getElementsByTagName("link")[0].textContent,
+      description: item.getElementsByTagName("description")[0].textContent,
+      pubDate: item.getElementsByTagName("pubDate")[0].textContent,
+    }));
+
+    return feed;
+  }
+}
