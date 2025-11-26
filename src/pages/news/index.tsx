9225f87df51a9212956800d576e6a57ce51128c1
FEAR939 <rogronin8877@gmail.com>
2025-11-26 20:40:22 +0100

    feat: use new rss feed

--- src/pages/news/index.tsx
+++ src/pages/news/index.tsx
@@ -2,13 +2,16 @@ import { h } from "../../lib/jsx/runtime";
 import { router } from "../../lib/router";
 import { Client } from "../../lib/anime2you/index";
 import { NewsCard } from "../../ui/newscard";
+import RSSClient from "@lib/rss";
 
 interface NewsQuery {}
 
 export default async function News(query: NewsQuery) {
-  const newsClient = new Client();
+  const rss = new RSSClient(
+    "https://cr-news-api-service.prd.crunchyrollsvc.com/v1/en-US/rss",
+  );
 
-  const news = await newsClient.getNews();
+  const news = (await rss.getRSSFeed()) || [];
 
   const page = (
     <div class="relative h-full w-full px-4 md:px-12 pt-12 pb-4 space-y-4 overflow-y-scroll">
