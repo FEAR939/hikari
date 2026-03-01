53d377bd60832b9d30d391e6d1f714f890a9230b
FEAR939 <rogronin88@gmail.com>
2026-03-01 01:58:17 +0100

    change: darker background color and now has a outline on hover for better visibility
    Signed-off-by: FEAR939 <rogronin88@gmail.com>

--- src/lib/components/common/Sidebar.svelte
+++ src/lib/components/common/Sidebar.svelte
@@ -12,7 +12,7 @@
     />
     <div class="space-y-2">
         <a
-            class={`block p-2 rounded-full cursor-pointer ${page.url.pathname === "/home/" ? "text-white" : "text-neutral-400"} hover:bg-white/5 focus-within:bg-white/5 transition-colors duration-150 outline-hidden`}
+            class={`block p-2 rounded-full cursor-pointer ${page.url.pathname === "/home/" ? "text-white" : "text-neutral-400"} ring-white/10 hover:bg-black/30 hover:backdrop-blur-lg hover:ring focus-within:bg-black/30 focus-within:backdrop-blur-lg focus-within:ring transition-colors duration-150 outline-hidden`}
             href="/home/"
             aria-label="home"
         >
@@ -35,7 +35,7 @@
         </a>
 
         <a
-            class={`block p-2 rounded-full cursor-pointer ${page.url.pathname === "/files/" ? "text-white" : "text-neutral-400"} hover:bg-white/5 focus-within:bg-white/5 transition-colors duration-150 outline-hidden`}
+            class={`block p-2 rounded-full cursor-pointer ${page.url.pathname === "/files/" ? "text-white" : "text-neutral-400"} ring-white/10 hover:bg-black/30 hover:backdrop-blur-lg hover:ring focus-within:bg-black/30 focus-within:backdrop-blur-lg focus-within:ring transition-colors duration-150 outline-hidden`}
             href="/files"
             aria-label="files"
         >
@@ -58,7 +58,7 @@
         </a>
 
         <a
-            class={`block p-2 rounded-full cursor-pointer ${page.url.pathname === "/news/" ? "text-white" : "text-neutral-400"} hover:bg-white/5 focus-within:bg-white/5 transition-colors duration-150 outline-hidden`}
+            class={`block p-2 rounded-full cursor-pointer ${page.url.pathname === "/news/" ? "text-white" : "text-neutral-400"} ring-white/10 hover:bg-black/30 hover:backdrop-blur-lg hover:ring focus-within:bg-black/30 focus-within:backdrop-blur-lg focus-within:ring transition-colors duration-150 outline-hidden`}
             href="/news"
             aria-label="news"
         >
