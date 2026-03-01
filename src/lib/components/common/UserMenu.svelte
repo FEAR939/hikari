4e607a6842cf695b530b68639d8817834f20ff9a
FEAR939 <rogronin88@gmail.com>
2026-03-01 21:32:05 +0100

    change: remove face in arrows and red sign out text
    Signed-off-by: FEAR939 <rogronin88@gmail.com>

--- src/lib/components/common/UserMenu.svelte
+++ src/lib/components/common/UserMenu.svelte
@@ -46,6 +46,8 @@
                             </div>
                         </div>
                     </div>
+
+                    <DropdownMenu.Separator />
                 {/if}
 
                 <DropdownMenu.Item
@@ -76,19 +78,6 @@
                         >
                     </div>
                     <div class=" self-center truncate">Watchlist</div>
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        height="24px"
-                        viewBox="0 -960 960 960"
-                        width="24px"
-                        fill="currentColor"
-                        class="absolute right-0 size-5 opacity-0 {$user
-                            ? 'group-hover:opacity-100 group-hover:right-4'
-                            : ''} transition-all duration-100"
-                        ><path
-                            d="M646-440H200q-17 0-28.5-11.5T160-480q0-17 11.5-28.5T200-520h446L532-634q-12-12-11.5-28t11.5-28q12-12 28.5-12.5T589-691l183 183q6 6 8.5 13t2.5 15q0 8-2.5 15t-8.5 13L589-269q-12 12-28.5 11.5T532-270q-11-12-11.5-28t11.5-28l114-114Z"
-                        /></svg
-                    >
                 </DropdownMenu.Item>
 
                 <DropdownMenu.Item
@@ -113,23 +102,12 @@
                         >
                     </div>
                     <div class=" self-center truncate">Settings</div>
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        height="24px"
-                        viewBox="0 -960 960 960"
-                        width="24px"
-                        fill="currentColor"
-                        class="absolute right-0 size-5 opacity-0 group-hover:opacity-100 group-hover:right-4 transition-all duration-100"
-                        ><path
-                            d="M646-440H200q-17 0-28.5-11.5T160-480q0-17 11.5-28.5T200-520h446L532-634q-12-12-11.5-28t11.5-28q12-12 28.5-12.5T589-691l183 183q6 6 8.5 13t2.5 15q0 8-2.5 15t-8.5 13L589-269q-12 12-28.5 11.5T532-270q-11-12-11.5-28t11.5-28l114-114Z"
-                        /></svg
-                    >
                 </DropdownMenu.Item>
 
+                <DropdownMenu.Separator />
+
                 <DropdownMenu.Item
-                    class="flex rounded-xl py-1.5 px-3 w-full {$user
-                        ? 'text-red-400'
-                        : 'text-white'} hover:bg-gray-50 dark:hover:bg-white/10 focus:bg-white/10 transition cursor-pointer outline-hidden"
+                    class="flex rounded-xl py-1.5 px-3 w-full text-white hover:bg-gray-50 dark:hover:bg-white/10 focus:bg-white/10 transition cursor-pointer outline-hidden"
                     onclick={async () => {
                         if ($user) {
                             await authClient.signOut();
