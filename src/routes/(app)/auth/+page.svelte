f0344fa87f1b8771b0fa83bf04a02e37bf037695
FEAR939 <rogronin88@gmail.com>
2026-03-05 20:36:21 +0100

    change: different UI
    Signed-off-by: FEAR939 <rogronin88@gmail.com>

--- src/routes/(app)/auth/+page.svelte
+++ src/routes/(app)/auth/+page.svelte
@@ -56,13 +56,13 @@
 </script>
 
 <div class="h-full w-full bg-black">
-    <div class="absolute inset-0 h-fit w-96 m-auto space-y-4">
+    <div class="absolute inset-0 h-fit w-md m-auto space-y-4">
         <div class="mb-12 space-y-2">
-            <div class="w-fit text-3xl font-semibold!">
-                {signState === "signup" ? "Sign Up" : "Sign In"}
+            <div class="w-full text-center text-2xl font-semibold!">
+                {signState === "signup" ? "Sign up" : "Sign in"} to Rune
             </div>
 
-            <div class="flex gap-2 text-sm w-fit">
+            <div class="w-full flex justify-center gap-2 text-sm">
                 {#if signState === "signup"}
                     <span class="text-gray-500">Already have an account?</span>
                     <button
@@ -88,75 +88,74 @@
                 in:slide={{ duration: 300, easing: quintOut }}
                 out:slide={{ duration: 300, easing: quintOut }}
             >
-                <div
-                    class="relative bg-white/5 backdrop-blur-lg px-6 py-3 rounded-full w-full before:content-[''] before:absolute before:inset-1 before:rounded-full hover:before:bg-white/10 focus-within:before:bg-white/10"
-                >
+                <div class="w-full space-y-1">
+                    <div>Username</div>
                     <input
                         bind:value={username}
                         type="text"
-                        placeholder="Username"
-                        class="relative z-1 outline-hidden placeholder:text-white"
+                        placeholder="Enter your Username"
+                        class="outline-hidden placeholder:text-gray-500 text-sm"
                     />
                 </div>
             </div>
         {/if}
         {#if ["signup", "signin"].includes(signState)}
-            <div
-                class="relative bg-white/5 backdrop-blur-lg px-6 py-3 rounded-full w-full before:content-[''] before:absolute before:inset-1 before:rounded-full hover:before:bg-white/10 focus-within:before:bg-white/10"
-            >
+            <div class="w-full space-y-1">
+                <div>Email</div>
                 <input
                     bind:value={email}
                     type="email"
-                    placeholder="Email"
-                    class="relative z-1 outline-hidden placeholder:text-white"
+                    placeholder="Enter your Email"
+                    class="outline-hidden placeholder:text-gray-500 text-sm"
                 />
             </div>
         {/if}
         {#if ["signup", "signin"].includes(signState)}
-            <div
-                class="relative bg-white/5 backdrop-blur-lg px-6 py-3 rounded-full w-full before:content-[''] before:absolute before:inset-1 before:rounded-full hover:before:bg-white/10 focus-within:before:bg-white/10"
-            >
-                <input
-                    bind:value={password}
-                    type={showPassword ? "text" : "password"}
-                    placeholder="Password"
-                    class="relative z-1 outline-hidden placeholder:text-white"
-                />
-                <button
-                    class="absolute right-4 top-1/2 transform -translate-y-1/2 outline-hidden cursor-pointer text-gray-500 focus:text-white hover:text-white transition-colors duration-300"
-                    onclick={() => (showPassword = !showPassword)}
-                >
-                    {#if showPassword}
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            height="24px"
-                            viewBox="0 -960 960 960"
-                            width="24px"
-                            fill="currentColor"
-                            class="size-5"
-                            ><path
-                                d="M480-320q75 0 127.5-52.5T660-500q0-75-52.5-127.5T480-680q-75 0-127.5 52.5T300-500q0 75 52.5 127.5T480-320Zm0-72q-45 0-76.5-31.5T372-500q0-45 31.5-76.5T480-608q45 0 76.5 31.5T588-500q0 45-31.5 76.5T480-392Zm0 192q-134 0-244.5-72T61-462q-5-9-7.5-18.5T51-500q0-10 2.5-19.5T61-538q64-118 174.5-190T480-800q134 0 244.5 72T899-538q5 9 7.5 18.5T909-500q0 10-2.5 19.5T899-462q-64 118-174.5 190T480-200Zm0-300Zm0 220q113 0 207.5-59.5T832-500q-50-101-144.5-160.5T480-720q-113 0-207.5 59.5T128-500q50 101 144.5 160.5T480-280Z"
-                            /></svg
-                        >
-                    {:else}
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            height="24px"
-                            viewBox="0 -960 960 960"
-                            width="24px"
-                            fill="currentColor"
-                            class="size-5"
-                            ><path
-                                d="M607-627q29 29 42.5 66t9.5 76q0 15-11 25.5T622-449q-15 0-25.5-10.5T586-485q5-26-3-50t-25-41q-17-17-41-26t-51-4q-15 0-25.5-11T430-643q0-15 10.5-25.5T466-679q38-4 75 9.5t66 42.5Zm-127-93q-19 0-37 1.5t-36 5.5q-17 3-30.5-5T358-742q-5-16 3.5-31t24.5-18q23-5 46.5-7t47.5-2q137 0 250.5 72T904-534q4 8 6 16.5t2 17.5q0 9-1.5 17.5T905-466q-18 40-44.5 75T802-327q-12 11-28 9t-26-16q-10-14-8.5-30.5T753-392q24-23 44-50t35-58q-50-101-144.5-160.5T480-720Zm0 520q-134 0-245-72.5T60-463q-5-8-7.5-17.5T50-500q0-10 2-19t7-18q20-40 46.5-76.5T166-680l-83-84q-11-12-10.5-28.5T84-820q11-11 28-11t28 11l680 680q11 11 11.5 27.5T820-84q-11 11-28 11t-28-11L624-222q-35 11-71 16.5t-73 5.5ZM222-624q-29 26-53 57t-41 67q50 101 144.5 160.5T480-280q20 0 39-2.5t39-5.5l-36-38q-11 3-21 4.5t-21 1.5q-75 0-127.5-52.5T300-500q0-11 1.5-21t4.5-21l-84-82Zm319 93Zm-151 75Z"
-                            /></svg
-                        >
-                    {/if}
-                </button>
+            <div class="w-full space-y-1">
+                <div>Password</div>
+                <div class="relative">
+                    <input
+                        bind:value={password}
+                        type={showPassword ? "text" : "password"}
+                        placeholder="Enter your Password"
+                        class="outline-hidden placeholder:text-gray-500 text-sm"
+                    />
+                    <button
+                        class="absolute right-4 top-1/2 transform -translate-y-1/2 outline-hidden cursor-pointer text-white transition-colors duration-300"
+                        onclick={() => (showPassword = !showPassword)}
+                    >
+                        {#if showPassword}
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                height="24px"
+                                viewBox="0 -960 960 960"
+                                width="24px"
+                                fill="currentColor"
+                                class="size-4"
+                                ><path
+                                    d="M764-84 624-222q-35 11-71 16.5t-73 5.5q-134 0-245-72T61-462q-5-9-7.5-18.5T51-500q0-10 2.5-19.5T61-538q22-39 47-76t58-66l-83-84q-11-11-11-27.5T84-820q11-11 28-11t28 11l680 680q11 11 11.5 27.5T820-84q-11 11-28 11t-28-11ZM480-320q11 0 21-1t20-4L305-541q-3 10-4 20t-1 21q0 75 52.5 127.5T480-320Zm0-480q134 0 245.5 72.5T900-537q5 8 7.5 17.5T910-500q0 10-2 19.5t-7 17.5q-19 37-42.5 70T806-331q-14 14-33 13t-33-15l-80-80q-7-7-9-16.5t1-19.5q4-13 6-25t2-26q0-75-52.5-127.5T480-680q-14 0-26 2t-25 6q-10 3-20 1t-17-9l-33-33q-19-19-12.5-44t31.5-32q25-5 50.5-8t51.5-3Zm79 226q11 13 18.5 28.5T587-513q1 8-6 11t-13-3l-82-82q-6-6-2.5-13t11.5-7q19 2 35 10.5t29 22.5Z"
+                                /></svg
+                            >
+                        {:else}
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                height="24px"
+                                viewBox="0 -960 960 960"
+                                width="24px"
+                                fill="currentColor"
+                                class="size-4"
+                                ><path
+                                    d="M607.5-372.5Q660-425 660-500t-52.5-127.5Q555-680 480-680t-127.5 52.5Q300-575 300-500t52.5 127.5Q405-320 480-320t127.5-52.5Zm-204-51Q372-455 372-500t31.5-76.5Q435-608 480-608t76.5 31.5Q588-545 588-500t-31.5 76.5Q525-392 480-392t-76.5-31.5ZM235.5-272Q125-344 61-462q-5-9-7.5-18.5T51-500q0-10 2.5-19.5T61-538q64-118 174.5-190T480-800q134 0 244.5 72T899-538q5 9 7.5 18.5T909-500q0 10-2.5 19.5T899-462q-64 118-174.5 190T480-200q-134 0-244.5-72Z"
+                                /></svg
+                            >
+                        {/if}
+                    </button>
+                </div>
             </div>
         {/if}
         {#if ["signup"].includes(signState)}
             <div
-                class="px-4 flex flex-wrap gap-x-4 text-xs"
+                class="flex flex-wrap gap-x-4 text-xs"
                 in:slide={{ duration: 300, easing: quintOut }}
                 out:slide={{ duration: 300, easing: quintOut }}
             >
@@ -205,9 +204,9 @@
                         break;
                 }
             }}
-            class="text-black px-4 py-3 mt-8 rounded-full w-full outline-hidden backdrop-blur-lg transition-colors duration-250 {hasValues
-                ? 'bg-white/90 hover:bg-gray-300/90 focus:bg-gray-300/90 cursor-pointer'
-                : 'bg-gray-500/90'}"
+            class="px-4 py-2.5 mt-4 rounded-full w-full outline-hidden transition-colors duration-100 text-sm {hasValues
+                ? 'bg-gray-100/5 hover:bg-gray-100/10 focus:bg-gray-100/10 text-white cursor-pointer'
+                : 'bg-gray-100/5 text-gray-800'}"
         >
             {signState === "signup" ? "Sign Up" : "Sign In"}
         </button>
